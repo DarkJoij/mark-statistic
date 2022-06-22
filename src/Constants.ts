bdb5050af02329ef33d118e76fad3f8f8bff27fd
Dallas <86653440+DarkJoij@users.noreply.github.com>
2022-06-22 11:16:39 +0300

    v. 1.0.0

---
+++ src/Constants.ts
@@ -0,0 +1,8 @@
+export const title = 'Your final exam statistic';
+export const bottom = 'Total result:';
+
+export const mainTableLine = '+{s}-------------------------------+';
+export const bodyTableLine = '+{s}+---------+---------+----------+';
+
+export const bodyWords: string[] = ['Integer', 'D. mark', 'Percents'];
+export const bodyWordsLength = [9, 9, 10];
