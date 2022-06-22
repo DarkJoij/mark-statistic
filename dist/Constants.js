09e0c3d6ec14e147e87f53fdb5983d35fe4d7d5e
Dallas <86653440+DarkJoij@users.noreply.github.com>
2022-06-22 11:17:21 +0300

    v. 1.0.0

---
+++ dist/Constants.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.bodyWordsLength = exports.bodyWords = exports.bodyTableLine = exports.mainTableLine = exports.bottom = exports.title = void 0;
+exports.title = 'Your final exam statistic';
+exports.bottom = 'Total result:';
+exports.mainTableLine = '+{s}-------------------------------+';
+exports.bodyTableLine = '+{s}+---------+---------+----------+';
+exports.bodyWords = ['Integer', 'D. mark', 'Percents'];
+exports.bodyWordsLength = [9, 9, 10];
